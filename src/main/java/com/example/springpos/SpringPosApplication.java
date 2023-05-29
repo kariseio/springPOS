2d4ccb33fd572de592efa586d6d089ee4743d6d2
kariseio <kariseio@naver.com>
2023-05-29 21:26:13 +0900

    Initial commit

---
+++ src/main/java/com/example/springpos/SpringPosApplication.java
@@ -0,0 +1,13 @@
+package com.example.springpos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPosApplication.class, args);
+    }
+
+}
