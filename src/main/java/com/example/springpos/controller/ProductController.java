340562564afd168718accdd88926547e0f30de92
kariseio <kariseio@naver.com>
2023-06-03 14:10:50 +0900

    컨트롤러 틀 잡기

---
+++ src/main/java/com/example/springpos/controller/ProductController.java
@@ -0,0 +1,8 @@
+package com.example.springpos.controller;
+
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ProductController {
+}
---
+++ src/main/java/com/example/springpos/controller/ReceiveController.java
@@ -0,0 +1,7 @@
+package com.example.springpos.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ReceiveController {
+}
