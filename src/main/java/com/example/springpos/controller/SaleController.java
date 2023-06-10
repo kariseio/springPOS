bd56d416fb85828d6daea6b444fe4b6d949b361b
kariseio <kariseio@naver.com>
2023-06-10 12:16:33 +0900

    판매 컨트롤러 업데이트

--- src/main/java/com/example/springpos/controller/SaleController.java
+++ src/main/java/com/example/springpos/controller/SaleController.java
@@ -1,17 +1,27 @@
 package com.example.springpos.controller;
 
+import com.example.springpos.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/sale")
 @Controller
 public class SaleController {
+    private ProductService productService;
 
-    @GetMapping("/")
-    public String sale() {
-        return "saleInfo/saleInfo";
+    @Autowired
+    public SaleController(ProductService productService) {
+        this.productService = productService;
     }
 
+
+    @GetMapping("/sale")
+    public String sale(Model model) {
+        model.addAttribute("productlist", productService.productList());
+
+        return "sale/sale";
+    }
 }
